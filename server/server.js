25bb44424b790cbf4d785a120cda4e122743545f
aagoos <alexhagoos@gmail.com>
2021-11-06 19:15:23 -0400

    changed server to use heroku's dynamic port assignment when not testing locally

--- server/server.js
+++ server/server.js
@@ -38,6 +38,7 @@ app.get("/lookup", (req, res) => {
 });
 
 //listen for requests
-app.listen(port, () => {
-    console.log(`Now listening for requests on port ${port}`);
+app.listen(port || process.env.PORT, () => {
+    let num = port || process.env.PORT; //to make heroku happy
+    console.log(`Now listening for requests on port ${num}`);
 })
