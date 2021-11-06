8291a498ad7f42a5c5913404d8350f1227302030
aagoos <alexhagoos@gmail.com>
2021-11-06 18:00:03 -0400

    fixed database.js exports

--- server/database.js
+++ server/database.js
@@ -2,33 +2,42 @@
 
 //wrapper for CRUD stuff
 //return all recipe results which match the query
-export function findAll(query){
+function findAll(query){
 
 }
 
 //return the first recipe result which matches the query, or undefined if there are no matches
-export function findFirst(query){
+function findFirst(query){
 
 }
 
 //same as findFirst, but specifies an ID directly
-export function find(id){
+function find(id){
 
 }
 
 //create a new recipe in the database
 //be careful of ID collisions
-export function insert(data){
+function insert(data){
 
 }
 
 //update or create a recipe (will replace a recipe with the same id)
 //be careful of ID collisions
-export function put(data){
+function put(data){
 
 }
 
 //delete a recipe with the specified id. Does nothing if the ID was not present.
-export function remove(id) {
+function remove(id) {
 
 }
+
+//exports
+module.exports.find = find;
+module.exports.findAll = findAll;
+module.exports.findFirst = findFirst
+
+module.exports.insert = insert;
+module.exports.put = put;
+module.exports.remove = remove;
