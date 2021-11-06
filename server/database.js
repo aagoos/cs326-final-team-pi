795618eba10b18d42efb4ec5f64c3c5eed74689b
aagoos <alexhagoos@gmail.com>
2021-11-06 18:22:04 -0400

    added dummy responses for get requests, added structure for other API calls

--- server/database.js
+++ server/database.js
@@ -1,9 +1,15 @@
 'use strict'
 
+const fs = require("fs");
+const path = require("path")
+
 //wrapper for CRUD stuff
 //return all recipe results which match the query
 function findAll(query){
-
+    //dummy response
+    //load all the test data and return it
+    const json = JSON.parse(fs.readFileSync(path.resolve(__dirname, "./testData.json"), "utf-8"));
+    return json;
 }
 
 //return the first recipe result which matches the query, or undefined if there are no matches
@@ -13,7 +19,15 @@ function findFirst(query){
 
 //same as findFirst, but specifies an ID directly
 function find(id){
-
+    //dummy response
+    //return recipe 0
+    const json = JSON.parse(fs.readFileSync(path.resolve(__dirname, "./testData.json"), "utf-8"));
+    for(let recipe of json.recipes){
+        if(recipe.id === 0){
+            return recipe;
+        }
+    }
+    return undefined;
 }
 
 //create a new recipe in the database
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 'use strict'
 const express = require("express");
+const db = require("./database")
 const app = express();
 const port = 8080;
 const client = "/../client"
@@ -7,6 +8,34 @@ const client = "/../client"
 //serve client files
 app.use(express.static(__dirname + client));
 
+//semi-complete code for GET requests to /recipes
+app.get("/recipes", (req, res) => {
+    //send test data for now
+    res.json(db.findAll("dummy request"));
+    res.end();
+});
+
+//placeholder for PUT, which will be create and update of CRUD
+app.put("/recipes", (req, res) => {
+    
+});
+
+//placeholder for POST, which will be the create of CRUD
+app.post("/recipes", (req, res) => {
+    
+});
+
+//placeholder for delete, which will be delete of CRUD
+app.delete("/recipes", (req, res) => {
+    
+});
+
+//semi-complete code for GET requests to /recipes
+app.get("/lookup", (req, res) => {
+    //send test data for now
+    res.json(db.find("dummy request"));
+    res.end();
+});
 
 //listen for requests
 app.listen(port, () => {
