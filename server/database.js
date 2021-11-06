b03fa853b05aaae45a0c20cad3ea467ac0edf1a6
aagoos <alexhagoos@gmail.com>
2021-11-06 17:07:09 -0400

    stubbed out database javascript

---
+++ server/database.js
@@ -0,0 +1,34 @@
+'use strict'
+
+//wrapper for CRUD stuff
+//return all recipe results which match the query
+export function findAll(query){
+
+}
+
+//return the first recipe result which matches the query, or undefined if there are no matches
+export function findFirst(query){
+
+}
+
+//same as findFirst, but specifies an ID directly
+export function find(id){
+
+}
+
+//create a new recipe in the database
+//be careful of ID collisions
+export function insert(data){
+
+}
+
+//update or create a recipe (will replace a recipe with the same id)
+//be careful of ID collisions
+export function put(data){
+
+}
+
+//delete a recipe with the specified id. Does nothing if the ID was not present.
+export function remove(id) {
+
+}
