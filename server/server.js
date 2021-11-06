a5c705c13a91f01b54c005e17c4d87f382c35f04
aagoos <alexhagoos@gmail.com>
2021-11-06 17:06:37 -0400

    basic server which servers html/css files

--- server/server.js
+++ server/server.js
@@ -1,1 +1,14 @@
-//placeholder
+'use strict'
+const express = require("express");
+const app = express();
+const port = 8080;
+const client = "/../client"
+
+//serve client files
+app.use(express.static(__dirname + client));
+
+
+//listen for requests
+app.listen(port, () => {
+    console.log(`Now listening for requests on port ${port}`);
+})
